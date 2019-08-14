9c0461841bc360f001c7bdbf216e6a9d89b43a2e
Hamza Ait Oumghar <hamza.aitoumghar.3@gmail.com>
2019-08-14 11:30:09 +0100

    action creators

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-import C from './constants';
-import appReducer from './store/reducers';
-import { createStore } from 'redux';
+import storeFactory from './store';
+import {addDay} from './store/action';
+import initialState from './initialState';
 
 
-
-const store = createStore(appReducer);
-
-const unsubsribeGoalLogger = store.subscribe(()=>{
-    console.log(`
-        Goal : ${store.getState().goal}
-    `);
-})
-
-setInterval(()=>{
-    store.dispatch({
-        type: C.SET_GOAL,
-        payload: Math.floor(Math.random()*100) 
-    })
-},250);
+const store = storeFactory(initialState);
 
 
-setTimeout(()=>{
-    unsubsribeGoalLogger();
-},3000);
+store.dispatch(
+    addDay('Heavenly','2016-12-22')
+);
---
+++ src/store/action.js
@@ -0,0 +1,23 @@
+import C from '../constants';
+
+export function addDay(resort,date,powder=false,backcountry=false){
+
+    return {
+        type : C.ADD_DAY,
+        payload: {resort,date,powder,backcountry}
+    }
+}
+
+
+export function removeDay(resort,date,powder=false,backcountry=false){
+
+    return {
+        type : C.REMOVE_DAY,
+        payload: date
+    }
+}
+
+export const setGoal = (goal)=>({
+    type :C.SET_GOAL,
+    payload: goal
+})
